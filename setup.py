eca3a3a0e26c959947caab9938ae41ad1e2deb1e
Sergey Grankin <sgrankin@gmail.com>
2014-01-23 01:26:05 -0500

    Try to make the PYPI page pretty

--- setup.py
+++ setup.py
@@ -12,6 +12,13 @@ except ImportError:
     print("Could not import Cython. Install `cython` and rerun.")
     sys.exit(1)
 
+long_description = open('README.md').read()
+try:
+  import pypandoc
+  long_description = pypandoc.convert(long_description, 'rst', format='md')
+except:
+  pass
+
 setup(
   name='spooky_hash',
   version='1.0',
@@ -19,6 +26,15 @@ setup(
   description='Python wrapper for SpookyHash V2',
   author='Sergey Grankin',
   url='https://github.com/sgrankin/spooky_hash',
+  long_description = long_description,
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Cython',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development',
+  ],
 
   setup_requires=['nose>=1.0'],
   include_package_data=True,
